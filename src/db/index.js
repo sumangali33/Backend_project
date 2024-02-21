f62ec0325ed822a5e17f56592145e1468f564c14
Sumangali33 <paldasumangali33@gmail.com>
2024-02-21 17:01:04 +0530

    connection to db succesfull commit

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async function () {
+    try {
+        console.log(process.env.MONGO_URI);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        console.log(`\n connection to database is succesful! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection error: " + error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,2 @@
+import connectDB from "./db/index.js";
+connectDB();
